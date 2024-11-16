ee2bb07ab6d6e5ba46271955bd664de8e93691d2
Brijesh Gupta <134288793+official-brijeshgupta@users.noreply.github.com>
2024-11-16 23:32:23 +0100

    Initial commit with basic setup

---
+++ src/main/java/com/learn/grapghql/GrapghqlApplication.java
@@ -0,0 +1,13 @@
+package com.learn.grapghql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrapghqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrapghqlApplication.class, args);
+	}
+
+}
